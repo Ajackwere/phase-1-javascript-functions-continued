1737f1190a01a1460618f9464cb6f484e434d71c
Ajackwere <austinejackwere@gmail.com>
2023-06-18 10:31:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(target = "roller-skate") {
+  return `This Saturday, I want to ${target}!`;
+}
+function mondayWork(target = "go to the office") {
+  return `This Monday, I will ${target}.`;
+}
+// function wrapAdjective(flair = "*", string2 = "a dedicated programmer") {
+//   return function (adjective = "special") {
+//     return `You are ${flair}${adjective}${flair}${string2}${flair}!`;
+//   };
+// }
+function wrapAdjective(flair = "*", string2 = "a dedicated programmer") {
+  const part1 = "You are";
+  return function (adjective = "special") {
+    return `${part1} ${flair}${adjective}${flair}!`;
+  };
+}
